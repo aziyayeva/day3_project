f926b7a4b96193fa80e7b40d459cc741cb400a81
Anna Zi <aziyayeva@gmail.com>
2021-05-22 11:20:37 -0400

    Hello World functionality

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,4 +1,13 @@
 public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Git is fun");
+        System.out.println("Let's learn Git");
+    }
 }
+
+
 
 
