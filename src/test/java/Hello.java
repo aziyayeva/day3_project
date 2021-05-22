22c83495bab213aabd4b83fcae51c223166dd1d8
Anna Zi <aziyayeva@gmail.com>
2021-05-22 11:23:51 -0400

    Added commit to Remote Repository

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -7,6 +7,7 @@ public class Hello {
         System.out.println("Let's learn Git");
 
         System.out.println("Commit to Local Repository");
+        System.out.println("Commit to Remote Repository");
     }
 }
 
